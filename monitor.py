c5647b203fb8587f18ff5e9a5a57cb4aee064b76
Isaac Darker <Isaac Darker>
2017-03-28 16:55:01 -0600

    Agrego ejemplo de uso de biblioteca psutil

--- monitor.py
+++ monitor.py
@@ -3,5 +3,6 @@ import psutil
 lstproc = psutil.pids()
 for i in lstproc:
     print(psutil.Process(i).name()+"  %d Bytes"%psutil.Process(i).memory_info().rss)
-    print(psutil.Process(i).memory_percent("rss"))
+    print("Porcentaje de uso de memoria: %f"%psutil.Process(i).memory_percent(memtype="rss"))
+    print("numero de hilos : %d"%psutil.Process(i).num_threads())
     print()
